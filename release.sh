e4a92521ac7219ba2c68317eb4112470790970c6
quanticc <quanticc@users.noreply.github.com>
2019-01-06 17:18:26 -0300

    Update release script

--- release.sh
+++ release.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 if [ -z "$1" ]
   then
     echo "Version argument missing"
     exit 1
 fi
-chmod +x gradlew
 ./gradlew -Pversion="$1" clean build -x test publishToMavenLocal bintrayUpload
