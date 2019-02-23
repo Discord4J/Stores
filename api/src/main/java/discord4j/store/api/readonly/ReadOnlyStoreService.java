ed6a86656a9a15827bdbedbad341c2d818f856b8
quanticc <quanticc@users.noreply.github.com>
2019-02-23 02:03:09 -0300

    Add read-only store classes

---
+++ api/src/main/java/discord4j/store/api/readonly/ReadOnlyStoreService.java
@@ -0,0 +1,67 @@
+/*
+ * This file is part of Discord4J.
+ *
+ * Discord4J is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Discord4J is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Discord4J. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package discord4j.store.api.readonly;
+
+import discord4j.store.api.Store;
+import discord4j.store.api.primitive.LongObjStore;
+import discord4j.store.api.readonly.primitive.ReadOnlyLongObjStore;
+import discord4j.store.api.service.StoreService;
+import discord4j.store.api.util.StoreContext;
+import reactor.core.publisher.Mono;
+
+import java.io.Serializable;
+
+public class ReadOnlyStoreService implements StoreService {
+
+    private final StoreService delegate;
+
+    public ReadOnlyStoreService(StoreService delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public boolean hasGenericStores() {
+        return delegate.hasGenericStores();
+    }
+
+    @Override
+    public <K extends Comparable<K>, V extends Serializable> Store<K, V> provideGenericStore(Class<K> keyClass,
+                                                                                             Class<V> valueClass) {
+        return new ReadOnlyStore<>(delegate.provideGenericStore(keyClass, valueClass));
+    }
+
+    @Override
+    public boolean hasLongObjStores() {
+        return delegate.hasLongObjStores();
+    }
+
+    @Override
+    public <V extends Serializable> LongObjStore<V> provideLongObjStore(Class<V> valueClass) {
+        return new ReadOnlyLongObjStore<>(delegate.provideLongObjStore(valueClass));
+    }
+
+    @Override
+    public Mono<Void> init(StoreContext context) {
+        return delegate.init(context);
+    }
+
+    @Override
+    public Mono<Void> dispose() {
+        return delegate.dispose();
+    }
+}
---
+++ api/src/main/java/discord4j/store/api/readonly/primitive/ReadOnlyLongObjStore.java
@@ -0,0 +1,95 @@
+/*
+ * This file is part of Discord4J.
+ *
+ * Discord4J is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Discord4J is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Discord4J. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package discord4j.store.api.readonly.primitive;
+
+import discord4j.store.api.primitive.LongObjStore;
+import discord4j.store.api.util.LongObjTuple2;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.io.Serializable;
+
+public class ReadOnlyLongObjStore<V extends Serializable> implements LongObjStore<V> {
+
+    private final LongObjStore<V> backing;
+
+    public ReadOnlyLongObjStore(LongObjStore<V> backing) {
+        this.backing = backing;
+    }
+
+    @Override
+    public Mono<Void> saveWithLong(long key, V value) {
+        return Mono.empty();
+    }
+
+    @Override
+    public Mono<Void> saveWithLong(Publisher<LongObjTuple2<V>> entryStream) {
+        return Mono.empty();
+    }
+
+    @Override
+    public Mono<V> find(long id) {
+        return backing.find(id);
+    }
+
+    @Override
+    public Flux<V> findInRange(long start, long end) {
+        return backing.findInRange(start, end);
+    }
+
+    @Override
+    public Mono<Void> delete(long id) {
+        return Mono.empty();
+    }
+
+    @Override
+    public Mono<Long> count() {
+        return backing.count();
+    }
+
+    @Override
+    public Mono<Void> delete(Publisher<Long> ids) {
+        return Mono.empty();
+    }
+
+    @Override
+    public Mono<Void> deleteAll() {
+        return Mono.empty();
+    }
+
+    @Override
+    public Flux<Long> keys() {
+        return backing.keys();
+    }
+
+    @Override
+    public Flux<V> values() {
+        return backing.values();
+    }
+
+    @Override
+    public Mono<Void> invalidate() {
+        return Mono.empty();
+    }
+
+    @Override
+    public Mono<Void> deleteInRange(long start, long end) {
+        return Mono.empty();
+    }
+}
